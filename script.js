4ae69f64b5c2eede82e2b684411ebd497af3603e
Isis252003 <isis@denkwijzer.nl>
2020-09-09 09:28:49 +0000

    versie 1 p5js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var balX = 50;
+var balY = 50;
+var speedX = 5;
+var speenY = 5;
 /**
  * setup
  * de code in deze functie wordt één keer uitgevoerd door
@@ -7,8 +11,7 @@ function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
 
-  // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
+
 }
 
 
@@ -18,9 +21,32 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+  // Kleur de achtergrond blauw, zodat je het kunt zien
+  background('blue');
+
   // stel vulkleur in
   fill(100, 100, 255);
 
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(balX, balY, 80, 80);
+
+  balX = balX + 1;
+  balY = balY + 1; 
+  
+  if (balX >1240){
+    speedX = speedX * -1
+  }
+
+  if (balY > 720){
+    speedY = speedY * -1;
+  }
+ 
+  if (balX< 0){
+   speedX = speedX * 1;
+  }
+
+  if (balY <0) {
+    speedY = speedY * 1;
+  }
 }
+
